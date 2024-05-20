3f08a17857dcffd830b618bc395550bd07be513c
Alessio Vulpinari <alessiovulpinari99@gmail.com>
2024-05-20 16:38:22 +0200

    Ex 2 and ex 3

--- src/Exercise_2.java
+++ src/Exercise_2.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Exercise_2 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Inserisci prima stringa: ");
+        String firstString =sc.nextLine();
+        System.out.println("Inserisci seconda stringa: ");
+        String secondString =sc.nextLine();
+        System.out.println("Inserisci terza stringa: ");
+        String thirdString = sc.nextLine();
 
+        System.out.println(firstString + " " + secondString + " " + thirdString);
+        System.out.println(thirdString + " " + secondString + " " + firstString);
 
     }
 
